b2622d0b4c5fd22ed87e9ccbc27e3149bb387659
Larissa Duarte <larissaduarte93@gmail.com>
2021-07-15 19:25:27 -0400

    Iniciando Projeto Spring Dio Bootcamp Santander

---
+++ src/main/java/com/dio/sisconp/SisconpApplication.java
@@ -0,0 +1,13 @@
+package com.dio.sisconp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisconpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisconpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/sisconp/SisconpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.sisconp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisconpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
