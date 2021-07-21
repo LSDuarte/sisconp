e6244d6769964d46db9006c6dc749a7d05f2cd78
Larissa Duarte <larissaduarte93@gmail.com>
2021-07-21 13:10:18 -0400

    Finalizando entidade Usuario

--- src/main/java/com/dio/sisconp/model/Usuario.java
+++ src/main/java/com/dio/sisconp/model/Usuario.java
@@ -1,15 +1,21 @@
 package com.dio.sisconp.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 import lombok.Data;
 
 @Data
 public class Usuario {
-	
+
 	private Long id;
 	private CategoriaUsuario categoriaUsuario;
 	private String nome;
 	private Empresa empresa;
 	private NivelAcesso nivelAcesso;
 	private JornadaTrabalho jornadaTrabalho;
-	
+	private BigDecimal tolerancia;
+	private LocalDateTime inicioJornada;
+	private LocalDateTime finalJornada;
+
 }
